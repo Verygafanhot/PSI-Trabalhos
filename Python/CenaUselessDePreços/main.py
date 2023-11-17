07c7e84e3d305b5f4916dbe07e939d8c87e856cc
Gafanhoto <verygafanhot@outlook.pt>
2023-11-17 15:32:12 +0000

    Adicionei o programa mais useless de sempre (dizer qual é o dos 3 itens mais baratos)

---
+++ Python/CenaUselessDePreços/main.py
@@ -0,0 +1,8 @@
+print("Exercicio 5")
+numero1 = int(input("Escreva o preco do primeiro item: "))
+numero2 = int(input("Escreva o preco do segundo item: "))
+numero3 = int(input("Escreva o preco do terceiro item: "))
+
+menor = min(numero1, numero2, numero3)
+
+print("Devera comprar o item cujo o preco é", menor)
